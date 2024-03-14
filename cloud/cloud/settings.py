633b16f11028f426a82c484f867c1d26d14ad62d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-03-14 10:29:15 +0200

    creating models in django

--- cloud/cloud/settings.py
+++ cloud/cloud/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'cloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'cloud44',
+        'USER': 'cloud',
+        'PASSWORD': 'iti123456789_',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
--- cloud/students/models.py
+++ cloud/students/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # when create field in model ---> its default --> not null
+    # unless you say it accepts null explicitly
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, null=True, unique=True)
+    image = models.CharField(max_length=100, null=True)
+    age = models.IntegerField(default=10, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
